814ed46afe0c55dd87a956d73d5f6fb38280072f
1522902011 <1522902011@qq.com>
2018-06-23 15:41:50 +0800

    0623

---
+++ js/061103.js
@@ -0,0 +1,20 @@
+/**
+ * Created by wengyimi on 2018/6/23.
+ */
+
+$(function(){
+
+    $('.img4').click(function(){
+        window.location.href='061101.html';
+
+    });
+    $('#bt2').click(function(){
+        window.location.href='061101.html';
+alert(1)
+    });
+
+
+
+
+
+});/*È«²¿*/
